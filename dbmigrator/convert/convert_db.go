5f13e1d620545d14f5ba8fd3426e3afe7cb053d7
khlee <khlee@nubes-bridge.com>
2020-06-02 00:29:13 +0900

    DB Migration : Log regId, RegName 추가

--- dbmigrator/convert/convert_db.go
+++ dbmigrator/convert/convert_db.go
@@ -608,8 +608,8 @@ func GetLogList(isComment int, deviceCode string, userId string, contents string
 			data.OldStatus = list.OldStatus
 			data.NewStatus = list.NewStatus
 			data.DeviceCode = deviceCode
-			//data.RegisterId = userId
-			//data.RegisterName = list.RegName
+			data.RegisterId = userId
+			data.RegisterName = list.RegName
 			data.RegisterDate = list.RegTime
 			lc = append(lc, data)
 		}
