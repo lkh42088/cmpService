af7826ae3d050eb41ed26f0241f3ab15e07fdd74
정병화 <bhjung@nubes-bridge.com>
2020-06-09 19:00:02 +0900

    refs #76 고객사 목록 구현

--- svcmgr/rest/rest_handler_user.go
+++ svcmgr/rest/rest_handler_user.go
@@ -65,3 +65,53 @@ func (h *Handler) GetUsersPage(c *gin.Context) {
 	fmt.Println("OK users:", len(users.Users))
 	c.JSON(http.StatusOK, users)
 }
+
+func getPagination(c *gin.Context) (p models.Pagination, err error){
+	rowsPerPage, err := strconv.Atoi(c.Param("rows"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": lib.RestAbnormalParam})
+		return
+	}
+	offset, err := strconv.Atoi(c.Param("offset"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": lib.RestAbnormalParam})
+		return
+	}
+	orderBy := c.Param("orderby")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": lib.RestAbnormalParam})
+		return
+	}
+	order := c.Param("order")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": lib.RestAbnormalParam})
+		return
+	}
+	p = models.Pagination{
+		TotalCount:  0,
+		RowsPerPage: rowsPerPage,
+		Offset:      offset,
+		OrderBy:     orderBy,
+		Order:       order,
+	}
+	return p, err
+}
+
+func (h *Handler) GetCompaniesPage(c *gin.Context) {
+	page, err := getPagination(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": lib.RestAbnormalParam})
+		return
+	}
+
+	fmt.Println("1. page:")
+	page.String()
+	companies, err := h.db.GetCompaniesPage(page)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	fmt.Println("2. page:")
+	companies.Page.String()
+	fmt.Println("OK users:", len(companies.Companies))
+	c.JSON(http.StatusOK, companies)
+}
