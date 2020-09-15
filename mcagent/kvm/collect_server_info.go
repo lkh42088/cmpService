e25a57a4a4c76460d29da40eadf6cafd74a5413c
khlee <khlee@nubes-bridge.com>
2020-09-15 09:37:42 +0900

    Baremetal sys info 를 위한 구조체 변경

--- mcagent/kvm/collect_server_info.go
+++ mcagent/kvm/collect_server_info.go
@@ -21,22 +21,26 @@ type NetInterface struct {
 }
 
 type SysInfo struct {
-	Idx             int    `gorm:"primary_key;column:idx;not null;auto_increment;comment:'INDEX'" json:"idx"`
-	Hostname        string `gorm:"column:hostname;not null;comment:'SERVER 이름'" json:"hostname"`
-	OS              string `gorm:"column:os;comment:'OS 명'" json:"os"`
-	Uptime          uint64 `gorm:"column:uptime;comment:'UPTIME'" json:"uptime"`
-	BootTime        uint64 `gorm:"column:boottime;comment:'SERVER BOOTTIME'" json:"bootTime"`
-	CpuCore         int    `gorm:"column:cpu_core;comment:'CPU Core 개수'" json:"cpuCore"`
-	CpuModel        string `gorm:"column:cpu_model;comment:'CPU 모델명'" json:"cpuModel"`
-	Platform        string `gorm:"column:platform;comment:'Platform'" json:"platform"`
-	PlatformVersion string `gorm:"column:platform_version;comment:'Platform 버전'" json:"platformVersion"`
-	KernelArch      string `gorm:"column:kernel_arch;comment:'KERNEL 아키텍처'" json:"kernelArch"`
-	KernelVersion   string `gorm:"column:kernel_version;comment:'KERNEL 버전'" json:"kernelVersion"`
-	IP              string `gorm:"column:ip;not null;comment:'SERVER IP'" json:"ip"`
-	IfName          string `gorm:"column:if_name;comment:'Interface Name'" json:"ifName"`
-	IfMac           string `gorm:"column:if_mac;comment:'Interface MAC'" json:"ifMac"`
-	MemTotal        int64  `gorm:"column:mem_total;comment:'MEMORY 용량'" json:"mem"`
-	DiskTotal       int64  `gorm:"column:disk_total;comment:'HDD DISK 용량'" json:"disk"`
+	Idx             int    `gorm:"primary_key;column:idx;not null;unsigned;auto_increment;comment:'INDEX'" json:"idx"`
+	Hostname        string `gorm:"type:varchar(50);column:hostname;not null;comment:'SYSTEM 이름'" json:"hostname"`
+	OS              string `gorm:"type:varchar(50);column:os;comment:'OS 명'" json:"os"`
+	Uptime          uint64 `gorm:"column:uptime;not null;unsigned;comment:'UPTIME'" json:"uptime"`
+	BootTime        uint64 `gorm:"column:boottime;not null;unsigned;comment:'SYSTEM BOOT TIME'" json:"bootTime"`
+	CpuCore         int    `gorm:"column:cpu_core;not null;unsigned;comment:'CPU Core 개수'" json:"cpuCore"`
+	CpuModel        string `gorm:"type:varchar(50);column:cpu_model;comment:'CPU 모델명'" json:"cpuModel"`
+	Platform        string `gorm:"type:varchar(50);column:platform;comment:'Platform'" json:"platform"`
+	PlatformVersion string `gorm:"type:varchar(50);column:platform_version;comment:'Platform 버전'" json:"platformVersion"`
+	KernelArch      string `gorm:"type:varchar(50);column:kernel_arch;comment:'KERNEL 아키텍처'" json:"kernelArch"`
+	KernelVersion   string `gorm:"type:varchar(50);column:kernel_version;comment:'KERNEL 버전'" json:"kernelVersion"`
+	IP              string `gorm:"type:varchar(15);column:ip;not null;comment:'SYSTEM IP'" json:"ip"`
+	IfName          string `gorm:"type:varchar(50);column:if_name;not null;comment:'Interface Name'" json:"ifName"`
+	IfMac           string `gorm:"type:varchar(50);column:if_mac;not null;comment:'Interface MAC'" json:"ifMac"`
+	MemTotal        int64  `gorm:"column:mem_total;not null;unsigned;comment:'MEMORY 용량'" json:"mem"`
+	DiskTotal       int64  `gorm:"column:disk_total;not null;unsigned;comment:'HDD DISK 용량'" json:"disk"`
+}
+
+func (SysInfo) TableName() string {
+	return "sysinfo_tb"
 }
 
 type SysInfoDetail struct {
@@ -50,14 +54,14 @@ type SysInfoDetail struct {
 	Net         []net.InterfaceStat    `json:interface`
 }
 
-func GetSysInfoLegacy() {
-	info := syscall.Sysinfo_t{}
-	err := syscall.Sysinfo(&info)
-
-	if err == nil {
-		fmt.Printf("sysinfo : %+v\n", info)
-	}
-}
+//func GetSysInfoLegacy() {
+//	info := syscall.Sysinfo_t{}
+//	err := syscall.Sysinfo(&info)
+//
+//	if err == nil {
+//		fmt.Printf("sysinfo : %+v\n", info)
+//	}
+//}
 
 func GetEnvVar() {
 	env := syscall.Environ()
