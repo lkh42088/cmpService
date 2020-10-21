db7f8e7795928a75544a0dc736d5af0ff015fe65
정병화 <bhjung@nubes-bridge.com>
2020-10-21 18:19:19 +0900

    implementing backup..

--- mcagent/cron/backup_cron.go
+++ mcagent/cron/backup_cron.go
@@ -9,7 +9,6 @@ func AddBackupCronByHoursMin(vmName, hour, min string) (id cron.EntryID, err err
 	cronTime := fmt.Sprintf("@every %sh%sm0s - Backup", hour, min)
 	id, err = CronSch.Cr.AddFunc(cronTime, func() {
 		fmt.Printf("Backup: %s - %s\n", vmName, cronTime)
-		SafeSnapshot(vmName, GetTimeWord(), cronTime)
 		SafeBackup(vmName, GetTimeWord(), cronTime)
 	})
 	return id, err
@@ -19,7 +18,6 @@ func AddBackupCronByMin(vmName, min string) (id cron.EntryID, err error){
 	cronTime := fmt.Sprintf("@every 0h%sm0s - Backup", min)
 	id, err = CronSch.Cr.AddFunc(cronTime, func() {
 		fmt.Printf("Backup: %s - %s\n", vmName, cronTime)
-		SafeSnapshot(vmName, GetTimeWord(), cronTime)
 		SafeBackup(vmName, GetTimeWord(), cronTime)
 	})
 	return id, err
