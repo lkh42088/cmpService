8724e55b6082149924ddf6fc596881a4239ced65
khlee <khlee@nubes-bridge.com>
2020-11-02 15:25:06 +0900

    Cron 등록 이상 문제 수정; NAS 기능 구현 중

--- mcagent/kvm/backup_cron.go
+++ mcagent/kvm/backup_cron.go
@@ -6,7 +6,7 @@ import (
 )
 
 func AddBackupCronByHoursMin(vmName, hour, min string) (cron.EntryID, error){
-	cronTime := fmt.Sprintf("@every %sh%sm0s - Backup", hour, min)
+	cronTime := fmt.Sprintf("@every %sh%sm0s", hour, min)
 	id, err := CronSch.Cr.AddFunc(cronTime, func() {
 		fmt.Printf("Backup: %s - %s\n", vmName, cronTime)
 		SafeBackup(vmName, GetTimeWord(), cronTime)
@@ -15,7 +15,7 @@ func AddBackupCronByHoursMin(vmName, hour, min string) (cron.EntryID, error){
 }
 
 func AddBackupCronByMin(vmName, min string) (cron.EntryID, error){
-	cronTime := fmt.Sprintf("@every 0h%sm0s - Backup", min)
+	cronTime := fmt.Sprintf("@every 0h%sm0s", min)
 	id, err := CronSch.Cr.AddFunc(cronTime, func() {
 		fmt.Printf("Backup: %s - %s\n", vmName, cronTime)
 		SafeBackup(vmName, GetTimeWord(), cronTime)
